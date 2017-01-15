f1ffebeb53693ddb07e152323fc35bd5cd31fdcd
Cecile Muller <git@wildpeaks.fr>
2017-01-15 23:21:44 +0100

    Added optional minification
    The `--env.debug` flag can be used with both "webpack" and
    "webpack-dev-server" to tell the config to skip minification.

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,62 @@
 /* eslint-env node */
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
-module.exports = {
-	target: 'web',
-	devtool: 'source-map',
-	entry: './src/application.js',
-	output: {
-		path: path.resolve(__dirname, 'www'),
-		filename: 'bundle.js',
-		publicPath: ''
-	},
-	plugins: [
+module.exports = ({debug = false} = {}) => {
+	const plugins = [
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify(debug ? 'development' : 'production')
+		}),
 		new HtmlWebpackPlugin({
 			title: 'Example',
 			filename: 'index.html'
 		})
-	],
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				include: [
-					path.resolve(__dirname, 'src')
-				],
-				loader: 'babel-loader',
-				query: {
-					compact: true,
-					presets: [
-						['es2015', {modules: false}]
-					]
+	];
+	if (!debug){
+		plugins.push(
+			new webpack.optimize.UglifyJsPlugin({
+				sourceMap: 'source-map',
+				compress: {
+					warnings: false
+				},
+				output: {
+					comments: false
 				}
-			}
-		]
-	},
-	performance: {
-		hints: false
+			})
+		);
 	}
+
+	return {
+		target: 'web',
+		devtool: 'source-map',
+		entry: './src/application.js',
+		output: {
+			path: path.resolve(__dirname, 'www'),
+			filename: debug ? 'bundle.js' : 'bundle.min.js',
+			publicPath: ''
+		},
+		plugins,
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					include: [
+						path.resolve(__dirname, 'src')
+					],
+					loader: 'babel-loader',
+					query: {
+						compact: true,
+						presets: [
+							['es2015', {modules: false}]
+						]
+					}
+				}
+			]
+		},
+		performance: {
+			hints: false
+		}
+	};
 };
